43f693d64a8911c746782acc1f568b54c5ae4576
Elizar <elizarelayzo@gmail.com>
2025-02-25 18:44:43 +0300

    new commit

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -2,10 +2,9 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.DAO.CarDao;
+import web.dao.CarDao;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
